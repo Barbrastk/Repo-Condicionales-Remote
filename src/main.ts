414d7d9504f87ab818f044f4d5c06c97faec6874
Bárbara Starck <147934490+Barbrastk@users.noreply.github.com>
2025-12-19 13:27:40 +0100

    changes on main

--- src/main.ts
+++ src/main.ts
@@ -21,11 +21,40 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   
     const mostrarCarta = (carta: number) => {
-      imgCarta.src =
-        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/" +
-        carta +
-        "_copas.jpg";
-    };
+        switch (carta) {
+          case 1:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
+            break;
+          case 2:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
+            break;
+          case 3:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
+            break;
+          case 4:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
+            break;
+          case 5:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
+            break;
+          case 6:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
+            break;
+          case 7:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
+            break;
+          case 10:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
+            break;
+          case 11:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
+            break;
+          case 12:
+            imgCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+            break;
+        }
+      };
+      
   
     const terminarJuego = () => {
       terminado = true;
